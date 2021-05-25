afc0c2ba62cb42ba1fff62a6155644e9381f4deb
Timothee Schaeffer <timo.sch@hotmail.fr>
2021-05-25 17:12:43 +0200

    Added Gamma Filename in param dict

--- src/param.py
+++ src/param.py
@@ -21,10 +21,18 @@ def source_par():
     return Bunch(par)
 
 
+def table_par():
+    par = {
+        "import_table": False,              # Whether or not to import an external Gamma table 
+        "recalculate_table": True,          # Not really needed
+        "filename_table": None,             # Filename of the table to import OR filename to write in
+        }
+    return Bunch(par)
 
 
 def par():
     par = Bunch({
         "source": source_par(),
+        "table": table_par(),
         })
     return par
