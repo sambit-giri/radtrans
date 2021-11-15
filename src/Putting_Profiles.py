f652e9a78589694f963d1698e61b75baa645f91f
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2021-11-15 13:25:03 +0100

    Added Putting Profiles

--- src/Putting_Profiles.py
+++ src/Putting_Profiles.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import fftconvolve
-from astropy import units as u
+import pickle
 from astropy.cosmology import Planck15 as cosmo
 from astropy import constants as const
 from skimage.measure import label
@@ -184,6 +184,92 @@ for i in range(1, Nbr_regions):
         # print('we distributed : ',sum_distributed_xion==initial_excess)
 
 print('i: ', i, 'sum: ', np.sum(Grid))
+
+
+
+
+
+def Spreading_Excess(Grid_Storage):
+    Grid = np.copy(Grid_Storage)
+    nGrid = Grid.shape[0]
+    Binary_Grid = np.copy(Grid)
+    Binary_Grid[np.where(Grid < 0.999)] = 0
+    Binary_Grid[np.where(Grid >= 0.999)] = 1
+    connected_regions = label(Binary_Grid)
+
+    Nbr_regions = np.max(connected_regions) + 1
+
+    Grid_of_1 = np.full(((nGrid, nGrid, nGrid)), 1)
+    Grid_of_0 = np.zeros((nGrid, nGrid, nGrid))
+
+    # When i = 0, the region if the full region outside the bubbles
+    X_Ion_Tot_i = np.sum(Grid)
+    print('initial sum of ionized fraction :', np.sum(Grid))
+
+    for i in range(1, Nbr_regions):
+        connected_indices = np.where(connected_regions == i)
+        Grid_connected = np.copy(Grid_of_0)  ## Grid with the fiducial value only for the region i.
+        Grid_connected[connected_indices] = Grid[connected_indices]
+        ## take sub grid with only the connected region, find pixels where xion>1, sum the excess, and set these pixels to 1.
+        overlap = np.where(Grid_connected > 1)
+
+        excess_ion = np.sum(Grid_connected[overlap] - 1)
+        initial_excess = excess_ion
+        Grid[overlap] = 1
+
+        Inverted_grid = np.copy(Grid_of_1)
+        Inverted_grid[connected_indices] = 0
+
+        sum_distributed_xion = 0
+
+        if excess_ion > 0:
+            dist_from_boundary = distance_transform_edt(Inverted_grid)
+            dist_from_boundary[np.where(dist_from_boundary == 0)] = 2 * nGrid  ### eliminate pixels inside boundary
+            dist_from_boundary[np.where(Grid > 1)] = 2 * nGrid  ### eliminate pixels that already have excess x_ion (belonging to another connected regions..)
+            minimum = np.min(dist_from_boundary)
+            boundary = np.where(dist_from_boundary == minimum)  # np.where((dist_from_boundary == minimum )& ( Grid<1))
+
+            if np.sum(1 - Grid[boundary]) > excess_ion:  # if their is room for the excess ion,
+                                                #  you add in each cell a fraction of the neutral fraction available.
+                Grid[boundary] += (1 - Grid[boundary]) * excess_ion / np.sum(1 - Grid[boundary])
+                if np.any(Grid[boundary] > 1):
+                    print('1. Thats where we trigger')
+                sum_distributed_xion += excess_ion
+            else:
+                #print('have to go for more than 1 layer')
+                while np.sum(1 - Grid[boundary]) < excess_ion:
+                    sum_distributed_xion += np.sum(1 - Grid[boundary])
+                    excess_ion = excess_ion - np.sum(1 - Grid[boundary])
+                    Grid[boundary] = 1
+                    dist_from_boundary[boundary] = nGrid * 2            ### exclude this layer for next step
+                    minimum = np.min(dist_from_boundary)
+                    boundary = np.where(dist_from_boundary == minimum)  ### new closest region to fill with excess ion
+                # you go out of the *while* when np.sum(1 - Grid[boundary]) > excess_ion
+                residual_excess = (1 - Grid[boundary]) * excess_ion / np.sum(1 - Grid[boundary])
+                Grid[boundary] += residual_excess
+                sum_distributed_xion += excess_ion
+
+                if np.any(Grid[boundary] > 1):
+                    print('2. Thats where we trigger', aaaa)
+                    break
+
+        if np.any(Grid[boundary] > 1):
+            print('okay thats it')
+            # print('we distributed : ',sum_distributed_xion==initial_excess)
+
+
+    print('i: ', i, 'sum: ', np.sum(Grid))
+    X_Ion_Tot_f = np.sum(Grid)
+    if int(X_Ion_Tot_f) != int(X_Ion_Tot_i) :
+        print('smtg is wrong when spreading xion_excess.')
+
+    return Grid
+
+
+
+
+
+
 
 
 
