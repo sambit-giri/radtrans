638dd53abb0301732e89e6da94bc0e790800a529
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2022-02-04 13:19:08 +0100

    fixed beug

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,6 @@ from . import *
 from .solver import Source_MAR
 from .param import par
 from .couplings import *
-from .run import run_solver, paint_profiles, compute_GS, grid_dTb
+from .run import *
+from .global_qty import xHII_approx
  
