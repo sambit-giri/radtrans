c590f6c122b6f4da84df5cf6cdbbb8217657dc87
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2022-02-14 18:11:38 +0100

    removed RK45 form init

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from .param import par
 from .couplings import *
 from .run import *
 from .global_qty import xHII_approx
-from .solver_RK45 import Source_MAR_RK45
+#from .solver_RK45 import Source_MAR_RK45
 from .run import *
-from .run_Ross19 import *
+#from .run_Ross19 import *
  
