6c7771689cf071e8f8c803e7e05bde643be22fe3
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2022-05-23 13:02:16 +0200

    added function to paint ioni map from emulator paint_ion_profile_emulator

--- src/emulator.py
+++ src/emulator.py
@@ -11,10 +11,11 @@ import pickle
 import datetime
 from radtrans.constants import cm_per_Mpc, sec_per_year, M_sun, m_H, rhoc0, Tcmb0
 from radtrans.astro import Read_Rockstar
-from radtrans.cosmo import T_adiab
+import matplotlib.pyplot as plt
 import os
 import copy
 from skopt import sampler
+from radtrans.profiles_on_grid import profile_to_3Dkernel, Spreading_Excess_Fast, put_profiles_group
 
 
 def run_RT_for_emul(Mhalo,fstar,parameters,Helium):
@@ -84,4 +85,102 @@ def gen_training_set(param,Sampling, Helium = True):
             Mhalo = 10 ** float(Sampling[i][0])
             fstar = Sampling[i][1]
             run_RT_for_emul(Mhalo,fstar,param,Helium)
+
+
+
+
+
+
+def paint_ion_profile_emulator(filename,param,fstar):
+    """
+    Paint the Tk, xHII and Lyman alpha profiles on a grid for a single snapshot named filename.
+
+    Parameters
+    ----------
+    param : dictionnary containing all the input parameters
+    filename : the name of the snapshot, contained in param.sim.halo_catalogs.
+
+    Returns
+    -------
+    Does not return anything. Paints and stores the grids on the directory grid_outputs.
+    """
+    catalog_dir = param.sim.halo_catalogs
+    starttimeprofile = datetime.datetime.now()
+    z_start = param.solver.z
+    M_Bin = np.logspace(np.log10(param.sim.M_i_min), np.log10(param.sim.M_i_max), param.sim.binn, base=10)
+
+    LBox = param.sim.Lbox  # Mpc/h
+    nGrid = param.sim.Ncell  # number of grid cells
+    catalog = catalog_dir + filename
+    halo_catalog = Read_Rockstar(catalog, Nmin=param.sim.Nh_part_min)
+    H_Masses, H_X, H_Y, H_Z, H_Radii = halo_catalog['M'], halo_catalog['X'], halo_catalog['Y'], halo_catalog['Z'], halo_catalog['R']
+    z = halo_catalog['z']
+
+    # Data
+    Sampling = pickle.load(file=open('./sampling_arr.pkl', 'rb'))
+    ## read in the first profile to get the redshift list
+    Mhalo = 10 ** float(Sampling[0][0])
+    profile = pickle.load(file=open('./profiles_output/dict_profiles_fst_0.1_zi25_Mh_1.0e+05.pkl','rb'))
+
+                                    #'dict_profiles_fst_' + str(round(Sampling[0][1], 2)) + '_zi{}_Mh_{:.1e}.pkl'.format(z_start, Mhalo),'rb'))
+    radial_grid = profile['r']
+
+    Indexing = np.argmin( np.abs(np.log10(H_Masses[:, None] / (M_Bin * np.exp(-param.source.alpha_MAR * (z - z_start))))), axis=1)
+    print('There are', H_Masses.size, 'halos at z=', z, )
+
+
+    ###load the emulator
+    reg_xi_load = pickle.load(open('./emul_xion_profile_z9.pkl', 'rb'))
+
+    if H_Masses.size == 0:
+        print('There is no sources')
+        Grid_xHII = np.array([0])
+
+    else:
+        Pos_Bubles = np.vstack((H_X, H_Y, H_Z)).T  # Halo positions.
+        Pos_Bubbles_Grid = np.array([Pos_Bubles / LBox * nGrid]).astype(int)[0]
+        Pos_Bubbles_Grid[np.where(Pos_Bubbles_Grid == nGrid)] = nGrid - 1  # you don't want Pos_Bubbles_Grid==nGrid
+        Grid_xHII = np.zeros((nGrid, nGrid, nGrid))
+        for i in range(len(M_Bin)):
+            indices = np.where(Indexing == i)  ## indices in H_Masses of halos that have an initial mass at z=z_start between M_Bin[i-1] and M_Bin[i]
+            if len(indices[0]) > 0:
+
+                x_HII_profile = reg_xi_load.predict([[np.log10(M_Bin[i]),fstar]])[0]
+                profile_xHII = interp1d(radial_grid * (1 + z), x_HII_profile, bounds_error=False, fill_value=(1, 0))
+                kernel_xHII = profile_to_3Dkernel(profile_xHII, nGrid, LBox)
+
+                #plt.semilogx(radial_grid * (1 + z), x_HII_profile,label = 'Mbin = {:.2e}'.format(M_Bin[i]))
+                #plt.legend()
+                if np.any(kernel_xHII > 0) :
+                    Grid_xHII += put_profiles_group(Pos_Bubbles_Grid[indices], kernel_xHII * 1e-7 / np.sum(kernel_xHII)) * np.sum(kernel_xHII) / 1e-7
+
+                endtimeprofile = datetime.datetime.now()
+                print(len(indices[0]), 'halos in mass bin ', i, 'took : ', endtimeprofile - starttimeprofile,'to paint profiles')
+
+        Grid_Storage = np.copy(Grid_xHII)
+
+        if np.sum(Grid_Storage) < nGrid ** 3:
+            Grid_xHII = Spreading_Excess_Fast(Grid_Storage)
+        else:
+            Grid_xHII = np.array([1])
+
+        endtimespread = datetime.datetime.now()
+        print('It took:', endtimespread - endtimeprofile, 'to spread the excess photons')
+
+        if np.all(Grid_xHII == 0):
+            Grid_xHII = np.array([0])
+
+        if np.all(Grid_xHII == 1):
+            print('universe is fully inoinzed. return 1 for Grid_xHII.')
+            Grid_xHII = np.array([1])
+
+
+    pickle.dump(file=open('./grid_output/xHII_Grid' + str(nGrid) + 'Emulator_' + '_snap' + filename[4:-5], 'wb'),obj=Grid_xHII)
+
+
+
+
+
+
+
 
