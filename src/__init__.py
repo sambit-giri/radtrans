3b2527afdb7b5122535676e44d831ebed22a9454
sambit-giri <sambitgiri@outlook.com>
2022-07-19 16:43:49 +0200

    dependencies updated

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from .solver import Source_MAR
 from .param import par
 from .couplings import *
 from .run import *
-from .global_qty import xHII_approx, tau_CMB
+from .global_qty import xHII_approx #, tau_CMB
 from .solver_Helium import Source_MAR_Helium
 from .solver_RK45 import Source_MAR_RK45
 from .run import *
