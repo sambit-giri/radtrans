c528252182e20ee298907827aa86367f41d1c258
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2022-11-16 11:24:02 +0100

    added convergence check and simplified hydrogen solver for comparison test

--- src/__init__.py
+++ src/__init__.py
@@ -9,4 +9,6 @@ from .solver_RK45 import Source_MAR_RK45
 from .simple_model import simple_solver
 from .emulator import gen_training_set, gen_Sampling
 from .python_functions import *
+from .solver_profile_H_simple import Source_H_ion_simple
+from .solver_with_redshifting import *
 #from .run_Ross19 import  *
