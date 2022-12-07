7ad4eeb3df6f46f404774f36beb0523119274e77
timothee.schaeffer <timothee.schaeffer@uzh.ch>
2022-12-07 13:14:48 +0100

    added the simple solver faster

--- src/__init__.py
+++ src/__init__.py
@@ -10,5 +10,8 @@ from .simple_model import simple_solver
 from .emulator import gen_training_set, gen_Sampling
 from .python_functions import *
 from .solver_profile_H_simple import Source_H_ion_simple
-#from .solver_with_redshifting import *
+
+from .simple_model_faster import simple_solver_faster
+
+
 #from .run_Ross19 import  *
